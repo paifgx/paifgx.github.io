6a55d4b725ff7b0e6b4b1129697a49bd20df9ba7
Patrik Garten <info@patrikgarten.de>
2023-05-16 00:49:00 +0200

    feat: change hero text

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -10,11 +10,15 @@
     <p
       class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
-      Ich bin Patrik Garten, ein in Deutschland ansässiger Entwickler und
-      Student an der Universität für angewandte Wissenschaften und Künste
-      Dortmund, mit einer Leidenschaft zum erlernen neuer Technik. Ich bin
-      spezialisiert auf Web- und App-Entwicklung mit TypeScript, JavaScript,
-      Kotlin und PHP.
+      Als freiberuflicher Informatiker bin ich mit Begeisterung auf einer
+      Mission sinnvolle Arbeit zu leisten die einen Unterschied macht - vor,
+      während und nach der Entwicklung. Ich betrachte meine Zusammenarbeit mit
+      Kunden als eine spannende Reise. Ganz gleich, ob es um IT-Beratung,
+      Webseitenerstellung, Entwicklung (mobiler) Apps oder die Automatisierung
+      von Prozessen und Geschäftsabläufen geht, stehe ich Ihnen mit meinem
+      fundierten Know-how als erfahrener Programmierer zur Seite. Dabei greife
+      ich auf eine breite Palette von Programmiersprachen und Frameworks zurück,
+      darunter besonders PHP, TypeScript/JavaScript, Kotlin/Java.
     </p>
   </div>
 </template>
