f8659e4344d5ecd963f9299293a3c58097bb98d7
Patrik Garten <info@patrikgarten.de>
2023-05-16 11:45:59 +0200

    feat: add profile and do some minor improvements

--- src/components/sections/SocialSection.vue
+++ src/components/sections/SocialSection.vue
@@ -7,29 +7,34 @@ import ActionButton from "@/components/ui/ActionButton.vue";
   <div class="flex flex-col gap-2 sm:flex-row justify-between items-center">
     <div class="flex flex-wrap gap-1">
       <social-icon
-        title="Github"
+        title="Profil von Patrik Garten auf www.github.com"
         link="https://github.com/paifgx"
         icon="akar-icons:github-fill"
       />
       <social-icon
-        title="Gitlab"
+        title="Profil von Patrik Garten auf www.gitlab.com"
         link="https://gitlab.com/paifgx"
         icon="mdi:gitlab"
       />
       <social-icon
-        title="Instagram"
+        title="Profil von Patrik Garten auf www.instagram.com"
         link="https://www.instagram.com/garten.dev"
         icon="akar-icons:instagram-fill"
       />
       <social-icon
-        title="LinkedIn"
+        title="Profil von Patrik Garten auf www.linkedin.com"
         link="https://www.linkedin.com/in/paifgx"
         icon="akar-icons:linkedin-box-fill"
       />
       <social-icon
-        title="Xing"
+        title="Profil von Patrik Garten auf www.xing.com"
         link="https://www.xing.com/profile/Patrik_Garten"
         icon="dashicons:xing"
+      />
+      <social-icon
+        title="Profil von Patrik Garten auf www.freelancermap.de"
+        link="https://www.freelancermap.de/freelancer-verzeichnis/profile/entwicklung/216540-profil-patrik-garten-software-engineer-full-stack-developer.html"
+        icon="dashicons:admin-site-alt3"
       />
     </div>
 
